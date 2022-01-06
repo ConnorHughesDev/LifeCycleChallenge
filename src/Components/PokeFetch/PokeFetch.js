59a205cd1d4bb8dad8e80bb9ed8813b600e22c6b
Connor Hughes <cj.hughes.dev@gmail.com>
2022-01-06 14:32:38 -0500

    actual first commit...

---
+++ src/Components/PokeFetch/PokeFetch.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import './PokeFetch.css';
+
+
+class PokeFetch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokeInfo: '',
+      pokeSprite: '',
+      pokeName: '',
+    }
+    this.state = { time: {}, seconds: 10 };
+      this.timer = 0;
+      this.startTimer = this.startTimer.bind(this);
+      this.countDown = this.countDown.bind(this);
+  }
+
+  fetchPokemon() {
+    let min = Math.ceil(1);
+    let max = Math.floor(152);
+    let pokeNum = Math.floor(Math.random() * (max - min) + min);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(res => {
+        this.setState({
+          pokeInfo: res,
+          pokeSprite: res.sprites.front_default,
+          pokeName: res.species.name,
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer === 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+    
+    // Check if we're at zero.
+    if (seconds === 0) { 
+      clearInterval(this.timer);
+      <style>
+        
+      </style>
+    }
+  }
+
+  render() {
+    return (
+      <div className={'wrapper'}>
+        <button className={'start'} onClick={() => this.fetchPokemon() & this.startTimer()}>Start!</button>
+        <h1 className={'timer'} >Timer Display</h1>
+        <h3>Timer: {this.state.seconds}</h3>
+        <div className={'pokeWrap'}>
+          <img className={'pokeImg'} src={this.state.pokeSprite} />
+          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PokeFetch;
+
+// filter: brightness(50%)
